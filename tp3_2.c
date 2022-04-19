3ba78f66aaa3ca58f03ff16b334e7eba0583435e
Diana Park <dianapark411@gmail.com>
2022-04-19 11:34:31 -0300

    Punto 4

--- tp3_2.c
+++ tp3_2.c
@@ -6,19 +6,26 @@ void cargarDatos(char *vec,int n);
 void mostrarDatos(char *vec, int n);
 
 int main(){
-    int n=5;
+    int n;
+
+    printf("\nEscriba la cantidad de nombres que desea ingresar: ");
+    scanf("%i", &n);
+
     char *Nombre;
     char *vector[n];
+
     Nombre = (char *)malloc(100*sizeof(char));
 
     for (int i = 0; i < n; i++){
-        printf("Ingrese los nombres: ");
+        fflush(stdin);
+        printf("\nIngrese los nombres: ");
         gets(Nombre);
+        fflush(stdin);
 
         vector[i]= (char *) malloc((strlen(Nombre+1)*sizeof(char)));
         strcpy(vector[i],Nombre);
     }
-    printf("Los nombres ingresados son: ");
+    printf("\nLos nombres ingresados son: \n");
     for (int i = 0; i < n; i++){
         puts(vector[i]);
     }
