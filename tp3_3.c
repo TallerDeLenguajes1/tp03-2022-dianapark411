058f27d4d8906155e09aa81e12383c83d32cf5d7
Diana Park <dianapark411@gmail.com>
2022-04-22 03:18:03 -0300

    Punto 3 terminado

--- tp3_3.c
+++ tp3_3.c
@@ -3,13 +3,13 @@
 #include <string.h>
 #include <time.h>
 
-struct Producto{
+struct Product{
     int ProductoID;
     int Cantidad;
     char *TipoProducto;
     float PrecioUnitario;
 };
-typedef struct Producto Producto;
+typedef struct Product Producto;
 
 struct Cliente{
     int ClienteID;
@@ -21,8 +21,10 @@ typedef struct Cliente Cliente;
 
 
 char *TiposProductos[] = {"Galletas", "Snack", "Cigarrillos", "Caramelos", "Bebidas"};
-void cargarDatosClientes(Cliente *Clientes, int cantClientes);
-void mostrarDatosClientes(Cliente *Clientes, int cantClientes);
+void cargarDatosClientes(Cliente *Client, int cantClientes);
+void mostrarProducto(Producto *pProducto);
+void mostrarDatosClientes(Cliente *Client, int cantClientes);
+float costoProducto(Producto *pProducto);
 
 
 int main(){
@@ -35,15 +37,17 @@ int main(){
     } while (cantClientes<1 || cantClientes>5);
 
     Cliente *Clientes;
-    Clientes = malloc(cantClientes*sizeof(Cliente));
-
+    Clientes = (Cliente *) malloc(cantClientes*sizeof(Cliente));
+ 
     cargarDatosClientes(Clientes, cantClientes);
+    mostrarDatosClientes(Clientes, cantClientes);
 
+    free(Clientes->NombreCliente);
     free(Clientes->Productos);
     free(Clientes);
 }
 
-void cargarDatosClientes(Cliente *Clientes, int cantClientes){
+void cargarDatosClientes(Cliente *Client, int cantClientes){
 
     int indice;
     char *buffer;
@@ -51,54 +55,92 @@ void cargarDatosClientes(Cliente *Clientes, int cantClientes){
 
     for (int i = 0; i < cantClientes; i++){
 
-        printf("\nCARGAR DATOS DEL CLIENTE %i: ", i);
+        printf("\nCARGAR DATOS DEL CLIENTE %i: ", i+1);
         
-        Clientes->ClienteID = i;
-
-        Clientes->NombreCliente = (char*)malloc((strlen(buffer+1)*sizeof(char)));
+        Client[i].ClienteID = i;
+        
         printf("\nIngrese el nombre del cliente: ");
-        fflush(stdin);
-        gets(Clientes->NombreCliente);    
-        fflush(stdin);
+        scanf(" %s", buffer);
+        int cantCaracteres = strlen(buffer+1);
+        Client[i].NombreCliente = (char*)malloc(( cantCaracteres *sizeof(char)));
+        strcpy(Client[i].NombreCliente, buffer);   
 
         srand((int)time(NULL));
-        Clientes->CantidadProductosAPedir = rand () % 5 + 1;
+        Client[i].CantidadProductosAPedir = 2; //rand () % 5 + 1
 
-        Clientes->Productos = malloc(Clientes->CantidadProductosAPedir*sizeof(Producto));
-        for (int j = 0; j < (Clientes->CantidadProductosAPedir); j++){
-            
-            printf("\nCARGAR DATOS DEL PRODUCTO %i: ", i);
+        //Producto * pProducto = Client->Productos = (Producto*)malloc(Client->CantidadProductosAPedir*sizeof(Producto));
+        Client[i].Productos = (Producto*)malloc(Client[i].CantidadProductosAPedir*sizeof(Producto));
 
-            Clientes->Productos->ProductoID= j;
-            fflush(stdin);
-            do{
-                printf("\nIngrese la cantidad (entre 1 y 10): ");
-                scanf("%i", &Clientes->Productos->Cantidad);
-            } while (Clientes->Productos->Cantidad < 1 || Clientes->Productos->Cantidad>10);
+        for (int j = 0; j < Client[i].CantidadProductosAPedir; j++){
             
+            printf("\n     CARGAR DATOS DEL PRODUCTO %i: ", j+1);
+
+            //DIFERENTES FORMAS DE MOVERSE
+            //(Client[i].Productos + j)->Cantidad = 0;
+            //pProducto[j].ProductoID= j;
+
+            Client[i].Productos[j].ProductoID = j;
+
             do{
-                printf("\nIngrese el tipo del producto(0 para Galletas, 1 para snack, 3 para Cigarrillos, 4 para Caramelos, 5 para Bebidas): ");
+                printf("\n     Ingrese la cantidad (entre 1 y 10): ");
+                scanf("%i", &Client[i].Productos[j].Cantidad);
+            } while (Client[i].Productos[j].Cantidad < 1 || Client[i].Productos[j].Cantidad>10);
+
+            do{
+                printf("\n     Ingrese el tipo del producto (0 para Galletas, 1 para snack, 2 para Cigarrillos, 3 para Caramelos, 4 para Bebidas): ");
                 scanf("%i", &indice);
             } while (indice < 0 || indice>5);
+
+            Client[i].Productos[j].TipoProducto = TiposProductos[indice];
             
-            Clientes->Productos->TipoProducto = TiposProductos[indice];
-
-
             do{
-                printf("\nEl precio por unidad (entre 10 y 100): ");
-                scanf("%f", &Clientes->Productos->PrecioUnitario)
-                ;
-            } while (Clientes->Productos->PrecioUnitario < 10 || Clientes->Productos->PrecioUnitario>100);
+                printf("\n     El precio por unidad (entre 10 y 100): ");
+                scanf("%f", &Client[i].Productos[j].PrecioUnitario);
+            } while (Client[i].Productos[j].PrecioUnitario < 10 || Client[i].Productos[j].PrecioUnitario>100);
 
-            Clientes->Productos ++;
         }
-        Clientes ++;
+        //Client ++; SI PODRIA USAR ESTA FORMA PARA MOVERME, CUANDO TENGO UNA ESTRUCTURA DENTRO DE OTRA NO CONVIENE
     }
-    
-    free(buffer); //  Elimino lo que reserve en esta funcion 
+    free(buffer); 
 }
 
 
-void mostrarDatosClientes(Cliente *Clientes, int cantClientes){
+void mostrarDatosClientes(Cliente *Client, int cantClientes){
+    float costoProd, costoTotal = 0;
 
+    for (int i = 0; i < cantClientes; i++){
+
+        printf("\n\n--------- DATOS DEL CLIENTE [%i] ---------- ", i+1);
+
+        printf("\nID del cliente: %i", Client[i].ClienteID);
+        printf("\nNombre del cliente:");
+        puts(Client[i].NombreCliente);
+        printf("Cantidad de productos a pedir: %i", Client[i].CantidadProductosAPedir);
+
+        Producto * pProducto =  Client[i].Productos;
+        for (int j=0; j < Client[i].CantidadProductosAPedir; j++){
+            
+            printf("\n       ---- DATOS DEL PRODUCTO [%i] ---- ", j+1);
+            mostrarProducto(pProducto);
+            costoProd = costoProducto(pProducto);
+            costoTotal = costoTotal + costoProd;
+            pProducto++;
+        }
+        printf("\nTotal a pagar por todos los productos: %f\n", costoTotal);
+    }  
+}
+
+void mostrarProducto(Producto *pProducto){
+            
+    printf("\n       ID del producto: %i", pProducto->ProductoID);
+    printf("\n       Cantidad: %i", pProducto->Cantidad);
+    printf("\n       Tipo: %s", pProducto->TipoProducto);
+    //puts(Client->Productos->TipoProducto);
+    printf("\n       Precio por unidad: %f", pProducto->PrecioUnitario);
+}
+
+float costoProducto(Producto *pProducto){
+    int costo = 0;
+    costo = pProducto->Cantidad * pProducto->PrecioUnitario;
+    return costo;
 }
